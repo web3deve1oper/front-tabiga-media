2bb11f794f995c45e1b1bb81073e1b23258574ad
Orazalinov Askhat <58249226+askesaske@users.noreply.github.com>
2021-10-21 19:40:54 +0300

    header and footer finished

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,92 @@
 <template>
   <footer class="footer">
-    FOOTER
+    <div class="footer__container">
+      <div class="footer__row">
+        <img src="../assets/img/footer-logo.svg" alt="" class="footer__logo">
+
+        <div class="footer__socials">
+          <a href="#" class="footer__social">
+            <svg width="35" height="35">
+              <use href="../assets/img/icons.svg#tg"></use>
+            </svg>
+          </a>
+
+          <a href="#" class="footer__social">
+            <svg width="35" height="35">
+              <use href="../assets/img/icons.svg#fb"></use>
+            </svg>
+          </a>
+
+          <a href="#" class="footer__social">
+            <svg width="35" height="35">
+              <use href="../assets/img/icons.svg#inst"></use>
+            </svg>
+          </a>
+
+          <a href="#" class="footer__social">
+            <svg width="35" height="35">
+              <use href="../assets/img/icons.svg#youtube"></use>
+            </svg>
+          </a>
+        </div>
+      </div>
+
+      <div class="footer__main">
+        <div class="footer__quote">
+          «Tabigat media» -онлайн журнал об охране природы
+        </div>
+
+        <div class="footer__link-box">
+          <div class="footer__title">
+            Общие ссылки
+          </div>
+
+          <div class="footer__links">
+            <div class="footer__link">О проекте</div>
+            <div class="footer__link">Политика конфиденциальности</div>
+            <div class="footer__link">Правовая информация</div>
+            <div class="footer__link">Обратная связь</div>
+          </div>
+        </div>
+
+        <div class="footer__link-box">
+          <div class="footer__title">
+            Рубрики
+          </div>
+
+          <div class="footer__links">
+            <div class="footer__link">Красная книга Казахстана</div>
+            <div class="footer__link">Наука</div>
+            <div class="footer__link">Советы</div>
+            <div class="footer__link">Бёрдвочинг</div>
+            <div class="footer__link">Мнения</div>
+            <div class="footer__link">Новости</div>
+            <div class="footer__link">Лица</div>
+          </div>
+        </div>
+
+        <div class="footer__active-box">
+          <div class="footer__title">
+            Есть вопросы или предложения?
+          </div>
+
+          <button class="footer__btn button button--white">
+            Написать нам
+          </button>
+        </div>
+      </div>
+
+      <div class="footer__rights">
+        © {{ new Date().getFullYear() }}, Tabigat media. Все права защищены
+      </div>
+    </div>
   </footer>
 </template>
 
 <script>
-export default {};
+export default {
+
+};
 </script>
 
 <style lang="scss" scoped>
--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,38 @@
 <template>
   <header class="header">
-    header
+    <div class="header__container">
+      <img src="../assets/img/header-logo.svg" alt="" class="header__logo">
+
+      <ul class="header__links">
+        <li class="header__link">Наука</li>
+        <li class="header__link">Дикая природа</li>
+        <li class="header__link">Советы</li>
+        <li class="header__link">Бёрдвочинг</li>
+        <li class="header__link">Мнения</li>
+        <li class="header__link header__link--red">Красная книга</li>
+      </ul>
+
+      <button class="header__btn button">
+        Подписаться
+      </button>
+
+      <div class="header__active-box">
+        <svg class="header__search" width="24" height="24">
+          <use href="../assets/img/icons.svg#search"></use>
+        </svg>
+
+        <svg class="header__burger" width="30" height="30">
+          <use href="../assets/img/icons.svg#burger"></use>
+        </svg>
+
+      </div>
+    </div>
   </header>
 </template>
 
 <script>
 export default {
-  methods: {
-
-  }
+  methods: {}
 };
 </script>
 
